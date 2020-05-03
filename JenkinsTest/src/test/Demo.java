970bf34a06e44de44c76039f3ed0f130346214fe
karishma <karishma.kumar@cyberinc.com>
2020-05-03 16:39:01 +0530

    "selenium run with bat file "

---
+++ JenkinsTest/src/test/Demo.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public static void DemoClass() {
+		
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\eclipse-workspace\\JenkinsTest\\driver\\chromedriver.exe");
+		 WebDriver driver=new ChromeDriver();  
+		 driver.navigate().to();
+		 System.out.println("welcome to learning");
+		 driver.quit();
+		 
+	}
+
+}
